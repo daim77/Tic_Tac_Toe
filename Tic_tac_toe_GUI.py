4f7a949633edd842579dc190bd5081ffb144f343
„Martin <martindanek4@gmail.com>
2021-01-12 17:44:11 +0100

    rename

--- Tic_tac_toe_GUI.py
+++ Tic_tac_toe_GUI.py
@@ -3,23 +3,27 @@ import numpy as np
 
 
 def create_board():
-    board = np.full((5, 5), 'X')
+    board = np.full((5, 5), 'Y')
     return board
 
 
 def game_gui(board):
     window = tk.Tk()
 
+    window.title('Tic Tac Toe')
+
     for i in range(5):
         for j in range(5):
             frame = tk.Frame(
                 master=window,
                 relief=tk.RAISED,
-                borderwidth=1
+                borderwidth=1,
+                height=500,
+                width=500
             )
             frame.grid(row=i, column=j)
-            label = tk.Label(master=frame, text=board[i][j])
-            label.pack()
+            button = tk.Button(master=frame, text=board[i][j])
+            button.pack()
 
     window.mainloop()
 
